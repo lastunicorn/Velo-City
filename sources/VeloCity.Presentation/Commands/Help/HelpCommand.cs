215a1e9a6d0e91d50faa8425520af860f7c1aa16
Alexandru Iuga <alex.iuga@gmail.com>
2022-04-20 19:23:01 +0300

    #33 - displayed the command parameters in the detailed help.

--- sources/VeloCity.Presentation/Commands/Help/HelpCommand.cs
+++ sources/VeloCity.Presentation/Commands/Help/HelpCommand.cs
@@ -17,6 +17,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using DustInTheWind.VeloCity.Presentation.Infrastructure;
 
@@ -31,7 +32,9 @@ namespace DustInTheWind.VeloCity.Presentation.Commands.Help
         [CommandParameter(DisplayName = "Command Name", Order = 1, IsOptional = true)]
         public string CommandName { get; set; }
 
-        public List<CommandInfo> Commands { get; private set; }
+        public List<CommandShortInfo> Commands { get; private set; }
+
+        public CommandFullInfo CommandDetails { get; private set; }
 
         public HelpCommand(AvailableCommands availableCommands)
         {
@@ -40,11 +43,114 @@ namespace DustInTheWind.VeloCity.Presentation.Commands.Help
 
         public Task Execute()
         {
-            Commands = availableCommands.GetOrderedCommandInfos()
-                .Where(x => x.IsEnabled)
-                .ToList();
+            if (CommandName != null)
+            {
+                IEnumerable<CommandInfo> allCommand = availableCommands.GetOrderedCommandInfos()
+                    .Where(x => x.IsEnabled);
+
+                CommandDetails = allCommand
+                    .Where(x => x.Name == CommandName)
+                    .Select(CreateCommandFullInfo)
+                    .FirstOrDefault();
+            }
+            else
+            {
+                IEnumerable<CommandInfo> allCommand = availableCommands.GetOrderedCommandInfos()
+                    .Where(x => x.IsEnabled);
+
+                Commands = allCommand
+                    .Select(x => new CommandShortInfo
+                    {
+                        Name = x.Name,
+                        Description = x.DescriptionLines.ToList()
+                    })
+                    .ToList();
+            }
 
             return Task.CompletedTask;
+        }
+
+        private static CommandFullInfo CreateCommandFullInfo(CommandInfo commandInfo)
+        {
+            return new CommandFullInfo
+            {
+                Name = commandInfo.Name,
+                Description = commandInfo.DescriptionLines.ToList(),
+                Usage = CreateUsage(commandInfo)
+            };
+        }
+
+        private static string CreateUsage(CommandInfo commandInfo)
+        {
+            StringBuilder sb = new($"velo {commandInfo.Name}");
+
+            IEnumerable<CommandParameterViewModel> ordinalParameters = commandInfo.ParameterInfos
+                .Where(x => x.Order != null)
+                .OrderBy(x => x.Order)
+                .Select(x => new CommandParameterViewModel(x));
+
+            foreach (CommandParameterViewModel parameterInfo in ordinalParameters)
+            {
+                sb.Append(' ');
+                sb.Append(parameterInfo);
+            }
+
+            IEnumerable<CommandParameterViewModel> namedParameters = commandInfo.ParameterInfos
+                .Where(x => x.Name != null || x.ShortName != 0)
+                .Select(x => new CommandParameterViewModel(x));
+
+            foreach (CommandParameterViewModel parameterInfo in namedParameters)
+            {
+                sb.Append(' ');
+                sb.Append(parameterInfo);
+            }
+
+            return sb.ToString();
+        }
+    }
+
+    internal class CommandParameterViewModel
+    {
+        private readonly CommandParameterInfo commandParameterInfo;
+
+        public CommandParameterViewModel(CommandParameterInfo commandParameterInfo)
+        {
+            this.commandParameterInfo = commandParameterInfo;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new();
+
+            char openChar = commandParameterInfo.IsOptional
+                ? '['
+                : '<';
+            sb.Append(openChar);
+
+            if (commandParameterInfo.Order != null)
+            {
+                sb.Append(commandParameterInfo.DisplayName.Replace(' ', '-'));
+            }
+            else
+            {
+                if (commandParameterInfo.Name != null)
+                    sb.Append($"-{commandParameterInfo.Name}");
+
+                if (commandParameterInfo.ShortName != null)
+                {
+                    if (commandParameterInfo.Name != null)
+                        sb.Append(" | ");
+
+                    sb.Append($"-{commandParameterInfo.ShortName}");
+                }
+            }
+
+            char closingChar = commandParameterInfo.IsOptional
+                ? ']'
+                : '>';
+            sb.Append(closingChar);
+
+            return sb.ToString();
         }
     }
 }
--- sources/VeloCity.Presentation/Commands/Help/HelpView.cs
+++ sources/VeloCity.Presentation/Commands/Help/HelpView.cs
@@ -15,7 +15,9 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using DustInTheWind.ConsoleTools.Controls.Tables;
 using DustInTheWind.VeloCity.Presentation.Infrastructure;
 
@@ -25,6 +27,14 @@ namespace DustInTheWind.VeloCity.Presentation.Commands.Help
     {
         public void Display(HelpCommand command)
         {
+            if (command.Commands is { Count: > 0 })
+                DisplayCommandsOverview(command.Commands);
+            else if (command.CommandDetails != null)
+                DisplayCommandDetails(command.CommandDetails);
+        }
+
+        private static void DisplayCommandsOverview(IEnumerable<CommandShortInfo> commands)
+        {
             Console.WriteLine("usage: velo [command]");
             Console.WriteLine();
             Console.WriteLine();
@@ -33,29 +43,35 @@ namespace DustInTheWind.VeloCity.Presentation.Commands.Help
 
             DataGrid dataGrid = new()
             {
-                Border = { IsVisible = false },
+                Border = { IsVisible = false }
             };
 
             Column column = dataGrid.Columns.Add(new Column());
             column.CellPaddingLeft = 0;
 
-            foreach (CommandInfo commandInfo in command.Commands)
-            {
-                ContentRow row = CreateContentRow(commandInfo);
-                dataGrid.Rows.Add(row);
-            }
+            IEnumerable<ContentRow> rows = commands.Select(CreateContentRow);
+            dataGrid.Rows.AddRange(rows);
 
             dataGrid.Display();
         }
 
-        private static ContentRow CreateContentRow(CommandInfo commandInfo)
+        private static ContentRow CreateContentRow(CommandShortInfo commandShortInfo)
         {
             ContentRow row = new();
 
-            row.AddCell(commandInfo.Name);
-            row.AddCell(commandInfo.DescriptionLines.ToList());
+            row.AddCell(commandShortInfo.Name);
+            row.AddCell(commandShortInfo.Description);
 
             return row;
+        }
+
+        private static void DisplayCommandDetails(CommandFullInfo commandDetails)
+        {
+            Console.WriteLine("usage:");
+            Console.WriteLine(commandDetails.Usage);
+
+            Console.WriteLine();
+            Console.WriteLine($"Description: {commandDetails.Description}");
         }
     }
 }
