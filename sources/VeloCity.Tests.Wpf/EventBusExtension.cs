849f3d93285c064a1a189b001fa6722f218c9a2f
Alexandru Iuga <alex.iuga@gmail.com>
2023-04-01 07:35:39 +0300

    moved wpf tests in separate assembly

--- sources/VeloCity.Tests.Wpf/EventBusExtension.cs
+++ sources/VeloCity.Tests.Wpf/EventBusExtension.cs
@@ -1,4 +1,4 @@
-// VeloCity
+﻿// VeloCity
 // Copyright (C) 2022 Dust in the Wind
 // 
 // This program is free software: you can redistribute it and/or modify
@@ -14,6 +14,14 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-global using FluentAssertions;
-global using Moq;
-global using Xunit;
+using DustInTheWind.VeloCity.Infrastructure;
+
+namespace DustInTheWind.VeloCity.Tests;
+
+internal static class EventBusExtension
+{
+    public static EventBusClient<T> CreateMockSubscriberFor<T>(this EventBus eventBus)
+    {
+        return new EventBusClient<T>(eventBus);
+    }
+}
