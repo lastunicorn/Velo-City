76ad9a288c93e412ea64eee847edbc16fefd4b58
Alexandru Iuga <alex.iuga@gmail.com>
2023-04-15 08:58:22 +0300

    update copyright information

--- sources/VeloCity.Tests.Unit.Wpf/Application/PresentSprintMemberCalendar/PresentSprintMemberCalendarUseCaseTests/HandleTests.cs
+++ sources/VeloCity.Tests.Unit.Wpf/Application/PresentSprintMemberCalendar/PresentSprintMemberCalendarUseCaseTests/HandleTests.cs
@@ -1,5 +1,5 @@
 // VeloCity
-// Copyright (C) 2022 Dust in the Wind
+// Copyright (C) 2022-2023 Dust in the Wind
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
