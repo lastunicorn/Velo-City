b5cfea4687ed95a984ba8606eb2fcae1ed88499e
Alexandru Iuga <alex.iuga@gmail.com>
2023-04-20 14:03:27 +0300

    added tests for the update sprint title use case.

---
+++ sources/VeloCity.Tests.Unit.Wpf/Application/UpdateSprintTitle/UpdateSprintTitleUseCaseTests/ConstructorTests.cs
@@ -0,0 +1,64 @@
+﻿// VeloCity
+// Copyright (C) 2022-2023 Dust in the Wind
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using DustInTheWind.VeloCity.Infrastructure;
+using DustInTheWind.VeloCity.Ports.DataAccess;
+using DustInTheWind.VeloCity.Wpf.Application.UpdateSprintTitle;
+
+namespace DustInTheWind.VeloCity.Tests.Unit.Wpf.Application.UpdateSprintTitle.UpdateSprintTitleUseCaseTests;
+
+public class ConstructorTests
+{
+    [Fact]
+    public void HavingNullUnitOfWork_WhenInstantiatingUseCase_ThenThrows()
+    {
+        EventBus eventBus = new();
+
+        Action action = () =>
+        {
+            _ = new UpdateSprintTitleUseCase(null, eventBus);
+        };
+
+        action.Should().Throw<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void HavingNullEventBus_WhenInstantiatingUseCase_ThenThrows()
+    {
+        Mock<IUnitOfWork> unitOfWork = new();
+
+        Action action = () =>
+        {
+            _ = new UpdateSprintTitleUseCase(unitOfWork.Object, null);
+        };
+
+        action.Should().Throw<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void HavingAllDependencies_WhenInstantiatingUseCase_ThenDoesNotThrow()
+    {
+        Mock<IUnitOfWork> unitOfWork = new();
+        EventBus eventBus = new();
+
+        Action action = () =>
+        {
+            _ = new UpdateSprintTitleUseCase(unitOfWork.Object, eventBus);
+        };
+
+        action.Should().NotThrow();
+    }
+}
